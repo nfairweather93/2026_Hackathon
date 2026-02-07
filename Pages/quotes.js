cdac2d368d720b5064bbf47d2ecee0089a03ca47
nfairweather93 <fairweather.3@wright.edu>
2026-02-07 03:58:43 -0500

    typo

--- Pages/quotes.js
+++ Pages/quotes.js
@@ -3,7 +3,7 @@ const quotes = [
   { text: `"Budgeting is just vibes with numbers."`, author: "Nathan Fairweather, 2026" },
   { text: `"If the pie chart looks scary, you're spending too much."`, author: "Aiden Cox, 1952" },
   { text: `"Overpaid? Underpaid? Let's start drama with data."`, author: "Pibble, 2026" },
-  { text: `"That professor makes HOW MUCH?`, author: "Every Student Ever, 2026"},
+  { text: `"That professor makes HOW MUCH?"`, author: "Every Student Ever, 2026"},
   {text: `"The numbers don't lie, but we might.."`, author: "Hackathon Ethics Committee, 2026"},
   {text: `"Wow, this is the coolest website I've ever seen!"`, author: "Some cool person, just now"},
   {text: `"Education is priceless. This salary, however, is not"`, author: "Joe Herman, 2026"},
